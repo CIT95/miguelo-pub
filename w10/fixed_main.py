df94dacb0a162c86cfeaec321c4546229281b002
Miguel Ontiveros <miguelontiveros27@gmail.com>
2022-03-16 23:05:38 -0700

    Found Bugs

--- w10/fixed_main.py
+++ w10/fixed_main.py
@@ -2,40 +2,40 @@
 
 # Starting inputs
 days = input("What day would you like to start with? ")
-miles = input("How many miles did you run on {days}? ")
+miles = float(input(f"How many miles did you run on {days}? "))
+
 
 # A list of dictionaries with day and miles that the functions will pull from.
-tracker = {
+tracker = [{
     "Day": days,
     "Miles": miles
-}
+},
+]
 
 
 # This adds new miles to the dictionary list.
-
-
 def new_mileage(tracker):
     program_continue = True
     while program_continue:
         next_value = input("would you like to continue? Type y for yes or"
-                           " n for no. ").lower())
+                           " n for no. ").lower()
         if next_value == "n":
             return False
         days = input("What's the next day? ")
         miles = float(input(f"How many miles did you run on {days}? "))
-        new_mile == []
+        new_mile = {}
         new_mile["Day"] = days
         new_mile["Miles"] = miles
         tracker.append(new_mile)
 
 
-new_mileage()
+new_mileage(tracker)
 
 # function gives the total miles.
 
 
 def add(tracker):
-    total = sum(item['Miles'])
+    total = sum(item['Miles']for item in tracker)
     return f"Your total mileage is: {total} miles."
 
 # function creates an average based off total miles/ the length of the
---
+++ w10/readme.md
@@ -0,0 +1,19 @@
+# Bug report for Kyle Lyman
+## Bug 1
+For line 5 the input should cast miles to a float.
+## Bug 2
+For line 5 an f-string should include an f before the quotes
+## Bug 3
+For line 5 there should be an ending parenthesis ")"
+## Bug 4
+for line 26 new_miles in new_mileage should be an assignemt opperator (=)
+## Bug 5
+For line 21 there is an extra ending parenthesis ")"
+## Bug 6
+For line 32 new_mileage() should pass tracker 
+## Bug 7
+For line 26 the array [] should be dictionary {} 
+## Bug 8
+For line 9 and 13 there should be a brackets [] since append wont work without it being a list
+## Bug 9
+For line 38 item is not defined since it is missing the for loop like in line 46.
